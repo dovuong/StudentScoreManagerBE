2f6e1bd03731d690d7f5e3dfbbb1504c264d4c43
dovuong <70367803+dovuong@users.noreply.github.com>
2022-11-19 09:23:07 +0700

    initial

---
+++ src/main/java/com/example/studentscoremanagerbe/StudentScoreManagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentscoremanagerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentScoreManagerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentScoreManagerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentscoremanagerbe/StudentScoreManagerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentscoremanagerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentScoreManagerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
